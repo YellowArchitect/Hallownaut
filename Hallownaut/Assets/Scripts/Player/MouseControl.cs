906096f6e57ab1ee32fd840d24aa602175e47611
Ben Schmidt <54752448+YellowArchitect@users.noreply.github.com>
2021-05-19 17:33:41 -0400

    Worked on player rotation following the mouse

--- Hallownaut/Assets/Scripts/Player/MouseControl.cs
+++ Hallownaut/Assets/Scripts/Player/MouseControl.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class MouseControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Tracks mouse position
+    Vector3 mousePos;
+    //Determines whether the player currently has control
+    bool hasControl = true;
+
+    private void Start()
     {
-        
+        print(Screen.width);
+        print(Screen.height);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasControl)
+        {
+            mousePos = Input.mousePosition;
+        }
+    }
+
+    //Rotates player based on mouse position
+    void FixedUpdate()
+    {
+        if (hasControl)
+        {
+            //Get the center of the screen
+            Vector3 screenCenter;
+            screenCenter.x = Screen.width / 2;
+            screenCenter.y = Screen.height / 2;
+
+            //Get the relative position
+            float adjacent = mousePos.x - screenCenter.x;
+            float opposite = mousePos.y - screenCenter.y;
+
+            //Get the rotation
+            Quaternion newRot = Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2(opposite, adjacent)*Mathf.Rad2Deg - 90);
+            transform.rotation = newRot;
+        }
+    }
+
+    //Enables player rotation
+    void enableControl()
+    {
+        hasControl = true;
+    }
+
+    //Disables player rotation
+    void disableControl()
+    {
+        hasControl = false;
     }
 }
