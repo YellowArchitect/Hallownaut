ea7551a25c57a7491650c7880b4712b4b9534ac8
Ben Schmidt <54752448+YellowArchitect@users.noreply.github.com>
2021-05-19 12:29:18 -0400

    Added basic movement

--- Hallownaut/Assets/Scripts/Player/PlayerMovement.cs
+++ Hallownaut/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,15 +7,49 @@ public class PlayerMovement : MonoBehaviour
     //Determines the movement speed
     public float moveSpeed = 1f;
 
-    // Start is called before the first frame update
+    //Determines whether the player currently has control
+    bool hasControl = true;
+    //USed to change position
+    float newX = 0f;
+    float newY = 0f;
+    Rigidbody2D rb;
+
+    //Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        
+        //If has control, record indicated movement
+        if (hasControl)
+        {
+            newX = Input.GetAxis("Horizontal");
+            newY = Input.GetAxis("Vertical");
+        }
+    }
+
+    //Updates physics component
+    void FixedUpdate()
+    {
+        Vector2 new_position = rb.position;
+        new_position.x += newX * Time.fixedDeltaTime * moveSpeed;
+        new_position.y += newY * Time.fixedDeltaTime * moveSpeed;
+        rb.position = new_position;
+        newX = newY = 0f;
+    }
+
+    //Enables player movement
+    void enableMovement()
+    {
+        hasControl = true;
+    }
+
+    //Disables player movement
+    void disableMovement()
+    {
+        hasControl = false;
     }
 }
