6edc77fa65bc42da30b86d0671a27aedbed7b716
Ben Schmidt <54752448+YellowArchitect@users.noreply.github.com>
2021-05-18 21:04:53 -0400

    Working on movement

--- Hallownaut/Assets/Scripts/Player/PlayerMovement.cs
+++ Hallownaut/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Determines the movement speed
+    public float moveSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
